54230acd5d8e00ad6e64b9eaf618dc6b9b6f8f5f
li <2633290579@qq.com>
2024-03-28 20:04:02 +0800

    fetchTry_untest

--- main/3_bookrak.js
+++ main/3_bookrak.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, View, Text, Button, ActivityIndicator } from 'react-native';
+import { TouchableOpacity, View, Text, Button, ActivityIndicator, StyleSheet } from 'react-native';
 import { FlatList, ScrollView } from 'react-native-gesture-handler';
 
 // function BookrakScreen({ navigation }) {
@@ -7,12 +7,63 @@ export default class BookrakScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isLoading: false,
-        }
+            userFavorID: [],
+            userNovel: [],
+            isLoading: true
+        };
     }
 
-    componentDidMount() {
-        const url = http;
+    componentDidMount = (route) => {
+        // const { id } = this.props.route.params;
+        const { userID } = 1;
+
+        const url_1 = `http://124.70.57.215:8000/user/${userID}`;
+        const url_2 = `http://124.70.57.215:8000/novel/${userFavorID}`
+        fetch(url_1)
+            .then((response) => response.json())
+            .then((json) => {
+                this.setState({ userFavorID: json.favors });
+            })
+            .catch((error) => console.error(error));
+
+        const { userFavorID } = this.state;
+        fetch(url_2)
+            .then((response) => response.json())
+            .then((json) => {
+                this.setState({ userNovel: json });
+            })
+            .catch((error) => console.error(error));
+        // .finally(() => {
+        //     this.setState({ isLoading: false});
+        // });
+    }
+
+    render() {
+        const { userNovel, isLoading } = this.state;
+        if (userNovel.length) {
+            return (
+                <View style={[styles.bookRak]}>
+                    <FlatList
+                        data={userNovel}
+                        keyExtractor={({ id }, index) => id}
+                        renderItem={({ item }) => (
+                            <View style={[styles.book]}>
+                                <Text style={[styles.favorName]}>{item.name}</Text>
+                            </View>
+                        )}
+                        ItemSeparatorComponent={() => {
+                            return <View style={[{ borderBottomWidth: 2 }, { borderBottomColor: 'grey' }, { marginVertical: 1 }]}></View>
+                        }}
+                        onEndReachedThreshold={0}
+                    />
+                </View>
+            );
+        }
+        if (!userNovel.length) {
+            return (
+                <Text>您暂时还未收藏书籍</Text>
+            )
+        }
     }
 
     // render() {
@@ -43,3 +94,10 @@ export default class BookrakScreen extends Component {
     //     );
     // }
 };
+
+const styles = StyleSheet.create({
+    book: {
+        backgroundColor: 'black',
+    },
+
+})
